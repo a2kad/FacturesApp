be33a80ec19d40098931c310a707cb1c2eb7d92b
a2kad <reshetnikovruslan705@gmail.com>
2023-12-12 17:43:03 +0100

    Navigation added

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet} from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen'
+
+const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
     return (
-        <View>
-            <Text>AppNavigation</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name='Home' component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
