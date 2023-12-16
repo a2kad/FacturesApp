842aa76e17922ee0b8133e33860c24bdbc64bd8d
a2kad <reshetnikovruslan705@gmail.com>
2023-12-16 20:54:22 +0100

    Login Screen added

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -1,19 +1,40 @@
-import { StyleSheet} from 'react-native'
+import { ActivityIndicator, StyleSheet} from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import useAuth from '../hooks/useAuth';
 import HomeScreen from '../screens/HomeScreen'
+import LoginScreen from '../screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='Home' component={HomeScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+    const {user, loading } = useAuth();
+
+    if (loading){
+        return(
+            <ActivityIndicator size='large' />
+            )
+    }else{
+        if (user){
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name='Home' component={HomeScreen} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            )
+        }else{
+            return (
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen name='Login' component={LoginScreen} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            )
+        }
+    }
+    
 }
 
 const styles = StyleSheet.create({})
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
+import React, { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/firebase';
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async () => {
+        if (email && password) {
+            try {
+                await signInWithEmailAndPassword(auth, email, password);
+            } catch (err) {
+                console.log('Got error', err.message);
+            }
+        }
+    }
     return (
         <View style={styles.container}>
             <Text>LoginScreen</Text>
+            <Text>E-mail</Text>
+            <TextInput style={styles.formField} value={email} onChangeText={value => setEmail(value)} />
+            <Text>Password</Text>
+            <TextInput style={styles.formField} value={password} onChangeText={value => setPassword(value)} />
+            <Pressable onPress={handleSubmit}>
+                <Text>Se connecter</Text>
+            </Pressable>
         </View>
     )
 }
@@ -15,5 +36,8 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    formField:{
+        backgroundColor: '#666'
     },
 })
