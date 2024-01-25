875f5cea03ec301a730ba9f1d1e1011df6799739
a2kad <reshetnikovruslan705@gmail.com>
2024-01-25 22:16:31 +0100

    Tab bar icons added

--- src/navigation/appNavigation.js
+++ src/navigation/appNavigation.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen'
 import DevisScreen from '../screens/DevisScreen';
+import { DocumentIcon, DocumentChartBarIcon } from "react-native-heroicons/solid";
 
 
 const Tab = createBottomTabNavigator();
@@ -12,8 +13,8 @@ export default function AppNavigation() {
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name='Facture' component={HomeScreen} />
-                <Tab.Screen name='Devis' component={DevisScreen} />
+                <Tab.Screen name='Facture' component={HomeScreen} options={{tabBarLabel: 'Factures', tabBarLabelStyle:{color:"grey"}, tabBarShowLabel: true, tabBarIcon: () =>(<DocumentIcon color={"grey"} size={24} />) }} />
+                <Tab.Screen name='Devis' component={DevisScreen} options={{tabBarLabel: 'Devis', tabBarLabelStyle:{color:"grey"}, tabBarShowLabel: true, tabBarIcon: () =>(<DocumentChartBarIcon color={"grey"} size={24} />) }} />
             </Tab.Navigator>
         </NavigationContainer>
     )
