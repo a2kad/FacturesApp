db9dba1a967aa6ac99cca313a0d6a39bed27476f
a2kad <reshetnikovruslan705@gmail.com>
2024-02-01 20:48:36 +0100

    Emmiteur text added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,34 +1,31 @@
-import { StyleSheet, Text, ScrollView, TextInput, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, ScrollView, TextInput, SafeAreaView, View } from 'react-native'
 import React from 'react'
 
 
 export default function HomeScreen() {
-    const [text, onChangeText] = React.useState('Useless Text');
+    const [text, onChangeText] = React.useState('');
     return (
         <SafeAreaView>
-            <ScrollView style={styles.container}>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.</Text>
-                    <TextInput style={styles.input} onChangeText={onChangeText} value={text}></TextInput>
-
+            <ScrollView>
+                <View  style={styles.inputLine}>
+                    <Text>Émetteur</Text>
+                    <TextInput style={styles.input} onChangeText={onChangeText} value={text} placeholder="Sélectionnez l'émetteur"></TextInput>
+                </View>
             </ScrollView>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        
+    inputLine: {
+        padding:10,
     },
     input: {
         height: 40,
         margin: 12,
         borderWidth: 1,
+        borderColor: '#CCC',
         padding: 10,
+        marginLeft: 0,
     },
 });
